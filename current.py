92f7fa6182508fc32108c95d9c2f1897054de0ff
skazi019 <skazi019@gmail.com>
2018-11-24 12:42:59 +0530

    Getting data and building basic models

---
+++ current.py
@@ -0,0 +1,26 @@
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('spambase.data').as_matrix()
+np.random.shuffle(data)
+
+X = data[:, :48]
+y = data[:, -1]
+
+Xtrain = X[:-100,]
+ytrain = y[:-100,]
+Xtest = X[-100:,]
+ytest = y[-100:,]
+
+
+model = MultinomialNB()
+model.fit(Xtrain, ytrain)
+print("\nAccuracy for NB: ",model.score(Xtest, ytest))
+
+
+from sklearn.ensemble import AdaBoostClassifier
+
+model = AdaBoostClassifier()
+model.fit(Xtrain, ytrain)
+print("Accuracy for Adaboost is: ", model.score(Xtest, ytest))
